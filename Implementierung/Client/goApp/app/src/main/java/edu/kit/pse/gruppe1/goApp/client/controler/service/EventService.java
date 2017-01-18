960191ca3757af55b588d4f921edbde12bec2a4d
Jonas <jonas@walai.de>
2017-01-18 21:07:39 +0100

    add java classes to Android project

---
+++ Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/controler/service/EventService.java
@@ -0,0 +1,44 @@
+package edu.kit.pse.gruppe1.goApp.client.controler.service;
+
+import edu.kit.pse.gruppe1.goApp.client.model.*;
+
+/**
+ * This Service provides methods to handle a single Event
+ */
+public class EventService {
+
+	/**
+	 * creates an event
+	 * @param name name of the group chosen freely by a member of the group
+	 * @param destination the location which the user choose on a map to let his event take place there (coordinates) and gave it a name (string)
+	 * @param eventAdmin the user who creates the event and is a group member
+	 * @param time the time when the event is going to take place. This time can not be in the past
+	 * @param group the group in which the event is created and which members are all invited
+	 * @return true, if method was successful, otherwise false
+	 */
+	private boolean create(String name, Location destination, User eventAdmin, Time time, Group group) {
+		// TODO - implement EventService.create
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * gets the event from the server database
+	 * @param eventID the id to identify which event to get
+	 * @return an event object with attributes
+	 */
+	private Event getEvent(int eventID) {
+		// TODO - implement EventService.getEvent
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * if the event admin whishes to change the event. Only time, destination and name can be changed
+	 * @param event the new instance of the event with the wanted attributes changed which will replace the old event
+	 * @return true, if method was successful, otherwise false
+	 */
+	private boolean change(Event event) {
+		// TODO - implement EventService.change
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/controler/service/GroupService.java
@@ -0,0 +1,85 @@
+package edu.kit.pse.gruppe1.goApp.client.controler.service;
+
+import edu.kit.pse.gruppe1.goApp.client.model.*;
+
+/**
+ * This Service provides methods to handle a single Group.
+ */
+public class GroupService {
+
+	/**
+	 * creates a new Group
+	 * @param name The name of the new group
+	 * @param founder The user who creates the group
+	 * @return true, if method was successful, otherwise false
+	 */
+	private boolean create(String name, User founder) {
+		// TODO - implement GroupService.create
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * deletes a group
+	 * @param group The group to be deleted
+	 *  
+	 * @return true, if methode was successful, otherwise false
+	 */
+	private boolean delete(Group group) {
+		// TODO - implement GroupService.delete
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * removes a member from the group
+	 * @param group The group in which the user currently is but will be deleted from
+	 * @param user The user who will be removed from the group
+	 * @return true, if method was successful, otherwise false
+	 */
+	private boolean deleteMember(Group group, User user) {
+		// TODO - implement GroupService.deleteMember
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * changes the name of the group
+	 * @param group The group which's founder changed the name
+	 * @param newName The new name of the group
+	 * @return true, if method was successful, otherwise false
+	 */
+	private boolean setName(Group group, String newName) {
+		// TODO - implement GroupService.setName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * gets the group from the server database
+	 * @param groupID The unique id of the group to find it
+	 * @return the group with the given id or null if it doesn't exist
+	 */
+	private Group getGroup(int groupID) {
+		// TODO - implement GroupService.getGroup
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * changes the founder of the group
+	 * @param group The group which founder changes
+	 * @param newFounder The user who gets the rights of the founder
+	 * @return true, if method was successful, otherwise false
+	 */
+	private boolean setFounder(Group group, User newFounder) {
+		// TODO - implement GroupService.setFounder
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * Returns all events which are associated with the group or null if no events exist in this group
+	 * @param group The existing group to get events from
+	 * @return all event in the group or null
+	 */
+	private List<pse.goApp.client.model.Event> getEvents(Group group) {
+		// TODO - implement GroupService.getEvents
+		throw new UnsupportedOperationException();
+	}
+
+}
