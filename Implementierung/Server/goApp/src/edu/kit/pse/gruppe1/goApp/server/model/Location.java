deba72968213224c58f4f1f1ad83f9d6180da051
Jonas <jonas@walai.de>
2017-01-18 20:08:24 +0100

    add Model classes to Server and corrected some errors

---
+++ Implementierung/Server/goApp/src/edu/kit/pse/gruppe1/goApp/server/model/Location.java
@@ -0,0 +1,32 @@
+package edu.kit.pse.gruppe1.goApp.server.model;
+
+/**
+ * The location is used to display something on the Map, whatever it is an user or the center of the group.
+ */
+public class Location {
+
+	/**
+	 * The longitude value of the position.
+	 */
+	private double longitude;
+	/**
+	 * The latitude value of the position.
+	 */
+	private double latitude;
+	/**
+	 * The name of a location helps the user to identify if the location is a center point of the group or the original meeting point.
+	 */
+	private String name;
+
+	/**
+	 * 
+	 * @param longitude
+	 * @param latitude
+	 * @param name
+	 */
+	public Location(double longitude, double latitude, String name) {
+		// TODO - implement Location.Location
+		throw new UnsupportedOperationException();
+	}
+
+}
