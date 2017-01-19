410b0e9af6b30ff14d9d52fb1d2bba72ef09d7eb
Tobias <tobias.ohlsson123@gmail.com>
2017-01-19 12:18:51 +0100

    Startet the activities

--- Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/view/StartActivity.java
+++ Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/view/StartActivity.java
@@ -1,4 +1,41 @@
 package edu.kit.pse.gruppe1.goApp.client.view;
 
-public class StartActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+     }
+
 }
