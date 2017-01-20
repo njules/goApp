cd6a0337fa3331cc130206cbd0eb714f237bb3e4
Katharina Riesterer <uxecu@student.kit.edu>
2017-01-20 16:56:17 +0100

    GroupService communication with the server?

--- Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/controler/service/GroupSearchService.java
+++ Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/controler/service/GroupSearchService.java
@@ -3,13 +3,19 @@ package edu.kit.pse.gruppe1.goApp.client.controler.service;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.List;
 
+import edu.kit.pse.gruppe1.goApp.client.controler.serverConnection.HTTPConnection;
 import edu.kit.pse.gruppe1.goApp.client.controler.serverConnection.JSONParameter;
 import edu.kit.pse.gruppe1.goApp.client.model.*;
 
@@ -21,10 +27,13 @@ public class GroupSearchService extends IntentService{
     public static final String TAG = GroupSearchService.class.getSimpleName();
     public static final String ERROR_INPUT = "wrong Input";
     public static final String ERROR = "Ups, Error occured";
+    private boolean running = false;
+    private static final String name = "GroupSearchService";
 
-    public GroupSearchService(String name) {
+    public GroupSearchService() {
         super(name);
     }
+
 
     /**
 	 * finds all groups which the user is a member of. This is used to present the groups in the StartActivity
@@ -32,19 +41,22 @@ public class GroupSearchService extends IntentService{
 
 	 */
 	public void getGroupsByMember(Context context,User user) {
-		if(user == null){
-            Log.i(TAG, ERROR_INPUT);
+		if(user == null) {
+            throw new IllegalArgumentException();
         }
         JSONObject requestJson = new JSONObject();
+
         try {
             requestJson.put(JSONParameter.UserID.toString(), user.getId());
         } catch (JSONException e) {
-            Log.i(TAG,ERROR);
+            e.printStackTrace();
         }
-        Intent requestIntent = new Intent(this,GroupSearchService.class);
+
+        Intent requestIntent = new Intent(context,GroupSearchService.class);
         requestIntent.putExtra("Json",requestJson.toString());
 
         startService(requestIntent);
+        running = true;
 
 
     }
@@ -61,7 +73,42 @@ public class GroupSearchService extends IntentService{
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-        Log.i(TAG,"Startet GroupSearchService");
+        Group[]groups = getGroups();
+        Intent resultIntent = new Intent();
+        Bundle groupBundle = new Bundle();
+        resultIntent.putExtra("groups",groupBundle);
+        LocalBroadcastManager manager = LocalBroadcastManager.getInstance(this.getApplicationContext());
+        manager.sendBroadcast(resultIntent);
+    }
 
-	}
+    public boolean isRunning() {
+        return running;
+    }
+
+    private Group[] getGroups(){
+        Group[] group = new Group[20];
+        for (int i = 0; i < 20 ; i++) {
+            group[i] = new Group(i,"name"+i);
+        }
+        return  group;
+    }
+
+    @Nullable
+    private Group[] getGroups(String json){
+        HTTPConnection connection = new HTTPConnection("GroupSearchServlet");
+        JSONObject result = connection.sendGetRequest(json);
+        try {
+           JSONArray jsons = result.getJSONArray(JSONParameter.GroupName.toString());
+            Group[] groups = new Group[jsons.length()];
+            for (int i = 0; i < jsons.length(); i++) {
+                groups[i] = new Group(
+                        (int)jsons.getJSONObject(i).get(JSONParameter.GroupID.toString()),
+                        (String)jsons.getJSONObject(i).get(JSONParameter.GroupName.toString()));
+                return groups;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
