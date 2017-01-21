c28c3bfc68b2ec7090d119d829a6cb80029db4bd
Katharina Riesterer <uxecu@student.kit.edu>
2017-01-21 23:42:44 +0100

    EventService

--- Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/controler/service/EventService.java
+++ Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/controler/service/EventService.java
@@ -2,45 +2,143 @@ package edu.kit.pse.gruppe1.goApp.client.controler.service;
 
 import java.sql.Date;
 
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import edu.kit.pse.gruppe1.goApp.client.controler.serverConnection.HTTPConnection;
+import edu.kit.pse.gruppe1.goApp.client.controler.serverConnection.JSONParameter;
 import edu.kit.pse.gruppe1.goApp.client.model.*;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * This Service provides methods to handle a single Event
  */
-public class EventService {
+public class EventService extends IntentService {
 
-	/**
-	 * creates an event
-	 * @param name name of the group chosen freely by a member of the group
-	 * @param destination the location which the user choose on a map to let his event take place there (coordinates) and gave it a name (string)
-	 * @param eventAdmin the user who creates the event and is a group member
-	 * @param time the time when the event is going to take place. This time can not be in the past
-	 * @param group the group in which the event is created and which members are all invited
-	 * @return true, if method was successful, otherwise false
-	 */
-	private boolean create(String name, Location destination, User eventAdmin, Date time, Group group) {
-		// TODO - implement EventService.create
-		throw new UnsupportedOperationException();
-	}
+    public static final String NAME = "EventService";
+    public static final String ACTION_CREATE = "CREATE";
+    public static final String ACTION_GET = "GET";
+    public static final String ACTION_CHANGE = "CHANGE";
+    public static final String SERVLET = "EventServlet";
 
-	/**
-	 * gets the event from the server database
-	 * @param eventID the id to identify which event to get
-	 * @return an event object with attributes
-	 */
-	private Event getEvent(int eventID) {
-		// TODO - implement EventService.getEvent
-		throw new UnsupportedOperationException();
-	}
 
-	/**
-	 * if the event admin whishes to change the event. Only time, destination and name can be changed
-	 * @param event the new instance of the event with the wanted attributes changed which will replace the old event
-	 * @return true, if method was successful, otherwise false
-	 */
-	private boolean change(Event event) {
-		// TODO - implement EventService.change
-		throw new UnsupportedOperationException();
-	}
 
+    public EventService() {
+        super(NAME);
+    }
+
+    /**
+     * creates an event
+     *
+     * @param name        name of the group chosen freely by a member of the group
+     * @param destination the location which the user choose on a map to let his event take place there (coordinates) and gave it a name (string)
+     * @param eventAdmin  the user who creates the event and is a group member
+     * @param time        the time when the event is going to take place. This time can not be in the past
+     * @param group       the group in which the event is created and which members are all invited
+     * @return true, if method was successful, otherwise false
+     */
+    public void create(Context context, String name, Location destination, User eventAdmin, Date time, Group group) {
+        JSONObject requestJson = new JSONObject();
+
+        try {
+            //TODO Add Destination and Time to Json
+            requestJson.put(JSONParameter.EventName.toString(), name);
+            requestJson.put(JSONParameter.GroupID.toString(), group.getId());
+            requestJson.put(JSONParameter.UserID.toString(), eventAdmin.getId());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Intent requestIntent = new Intent(context, GroupSearchService.class);
+        requestIntent.putExtra("Json", requestJson.toString());
+        requestIntent.setAction(ACTION_CREATE);
+
+        startService(requestIntent);
+    }
+
+    /**
+     * gets the event from the server database
+     *
+     * @param eventID the id to identify which event to get
+     * @return an event object with attributes
+     */
+    public void getEvent(Context context, int eventID) {
+        JSONObject requestJson = new JSONObject();
+
+        try {
+            requestJson.put(JSONParameter.EventID.toString(), eventID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Intent requestIntent = new Intent(context, GroupSearchService.class);
+        requestIntent.putExtra("Json", requestJson.toString());
+        requestIntent.setAction(ACTION_GET);
+
+        startService(requestIntent);
+    }
+
+    /**
+     * if the event admin whishes to change the event. Only time, destination and name can be changed
+     *
+     * @param event the new instance of the event with the wanted attributes changed which will replace the old event
+     * @return true, if method was successful, otherwise false
+     */
+    public void change(Context context, Event event) {
+        JSONObject requestJson = new JSONObject();
+
+        try {
+            //TODO add Eventattributes which should be able to be changed
+            requestJson.put(JSONParameter.EventID.toString(), event.getId());
+            requestJson.put(JSONParameter.EventName.toString(), event.getName());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Intent requestIntent = new Intent(context, GroupSearchService.class);
+        requestIntent.putExtra("Json", requestJson.toString());
+        requestIntent.setAction(ACTION_CHANGE);
+
+        startService(requestIntent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        HTTPConnection connection = new HTTPConnection(SERVLET);
+        Intent resultIntent = new Intent();
+        JSONObject result;
+        switch (intent.getAction()) {
+            case ACTION_CREATE: result = connection.sendPostRequest(intent.getStringExtra("JSON"));
+                resultIntent.setAction(intent.getAction());
+                try {
+                    //TODO what happens if error != 0
+                    resultIntent.putExtra("ERROR",result.getInt(JSONParameter.ErrorCode.toString()));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case ACTION_GET : result = connection.sendGetRequest(intent.getStringExtra("JSON"));
+                    //TODO add Event Attributes
+                    //Event event = new Event(result.getInt(JSONParameter.EventID.toString()),result.getString(JSONParameter.EventName.toString()));
+                    //TODO add Event to resultIntent
+                break;
+            case ACTION_CHANGE :  result = connection.sendPostRequest(intent.getStringExtra("JSON"));
+                try {
+                    //TODO what happens if error != 0
+                    resultIntent.putExtra("ERROR",result.getInt(JSONParameter.ErrorCode.toString()));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                break;
+            //TODO default case
+
+        }
+        resultIntent.setAction(intent.getAction());
+
+        LocalBroadcastManager manager = LocalBroadcastManager.getInstance(this.getApplicationContext());
+        manager.sendBroadcast(resultIntent);
+    }
 }
--- Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/controler/service/GroupSearchService.java
+++ Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/controler/service/GroupSearchService.java
@@ -27,6 +27,11 @@ public class GroupSearchService extends IntentService{
     public static final String TAG = GroupSearchService.class.getSimpleName();
     public static final String ERROR_INPUT = "wrong Input";
     public static final String ERROR = "Ups, Error occured";
+    public static final String ACTION_GETBYNAME = "GETBYNAME";
+    public static final String ACTION_GETBYUSER = "GETBYUSER";
+    public static final String SERVLET = "GroupSearchServlet";
+
+    //for testing
     private boolean running = false;
     private static final String name = "GroupSearchService";
 
@@ -54,6 +59,7 @@ public class GroupSearchService extends IntentService{
 
         Intent requestIntent = new Intent(context,GroupSearchService.class);
         requestIntent.putExtra("Json",requestJson.toString());
+        requestIntent.setAction(ACTION_GETBYUSER);
 
         startService(requestIntent);
         running = true;
@@ -66,36 +72,40 @@ public class GroupSearchService extends IntentService{
 	 * @param name the string which the user typed in the NewGroupActivity to find a new group he wants to be member of with that name
 	 * @return all groups the name is included in the group name or null
 	 */
-	private List<Group> getGroupsByName(String name) {
-		// TODO - implement GroupSearchService.getGroupsByName
-		throw new UnsupportedOperationException();
+	public void getGroupsByName(Context context, String name) {
+
+        JSONObject requestJson = new JSONObject();
+
+        try {
+            requestJson.put(JSONParameter.GroupName.toString(), name);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Intent requestIntent = new Intent(context,GroupSearchService.class);
+        requestIntent.putExtra("Json",requestJson.toString());
+        requestIntent.setAction(ACTION_GETBYNAME);
+
+        startService(requestIntent);
 	}
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-        Group[]groups = getGroups();
+        Group[]groups = getGroups(intent.getStringExtra("JSON"));
         Intent resultIntent = new Intent();
+        //TODO how to get the groups in an intent
         Bundle groupBundle = new Bundle();
         resultIntent.putExtra("groups",groupBundle);
+        resultIntent.setAction(intent.getAction());
+
         LocalBroadcastManager manager = LocalBroadcastManager.getInstance(this.getApplicationContext());
         manager.sendBroadcast(resultIntent);
     }
 
-    public boolean isRunning() {
-        return running;
-    }
-
-    private Group[] getGroups(){
-        Group[] group = new Group[20];
-        for (int i = 0; i < 20 ; i++) {
-            group[i] = new Group(i,"name"+i);
-        }
-        return  group;
-    }
 
     @Nullable
     private Group[] getGroups(String json){
-        HTTPConnection connection = new HTTPConnection("GroupSearchServlet");
+        HTTPConnection connection = new HTTPConnection(SERVLET);
         JSONObject result = connection.sendGetRequest(json);
         try {
            JSONArray jsons = result.getJSONArray(JSONParameter.GroupName.toString());
@@ -104,11 +114,23 @@ public class GroupSearchService extends IntentService{
                 groups[i] = new Group(
                         (int)jsons.getJSONObject(i).get(JSONParameter.GroupID.toString()),
                         (String)jsons.getJSONObject(i).get(JSONParameter.GroupName.toString()));
-                return groups;
             }
+            return groups;
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return null;
+    }
+//For testing
+    private Group[] getGroups(){
+        Group[] group = new Group[20];
+        for (int i = 0; i < 20 ; i++) {
+            group[i] = new Group(i,"name"+i);
+        }
+        return  group;
+    }
+//For testing
+    public boolean isRunning() {
+        return running;
     }
 }
