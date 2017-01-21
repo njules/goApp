cb30a031823d3defa3f8526ef0e603a83bfb24f6
Jonas <jonas@walai.de>
2017-01-21 11:53:03 +0100

    start with annotations in the model classes

--- Implementierung/Server/goApp/src/edu/kit/pse/gruppe1/goApp/server/model/Event.java
+++ Implementierung/Server/goApp/src/edu/kit/pse/gruppe1/goApp/server/model/Event.java
@@ -2,50 +2,88 @@ package edu.kit.pse.gruppe1.goApp.server.model;
 
 import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * An event is created by a user within a specific group.
  */
+@Entity
+@Table(name = "event")
 public class Event {
-    /**
-     * The name of an event is given by the creator.
-     */
-    private String name;
-    /**
-     * The Id is used to identify each event and is therefore unique.
-     */
-    private int id;
-    /**
-     * The time of an event tells when the event is starting and set by the creator of the event.
-     */
-    private Date time;
+	
+	/**
+	 * The Id is used to identify each event and is therefore unique.
+	 */
+	private Integer eventId;
+	
+	/**
+	 * The name of an event is given by the creator.
+	 */
+	private String name;
 
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
+	/**
+	 * The time of an event tells when the event is starting and set by the
+	 * creator of the event.
+	 */
+	private Date time;
+	
+	private Group group;
+	
 
 
-    /**
-     * @param id   The Id of the event.
-     * @param name The name of the event.
-     * @param time The time of the event.
-     */
-    public Event(int id, String name, Date time) {
-        this.id = id;
-        this.name = name;
-        this.time = time;
-        throw new UnsupportedOperationException();
-    }
+	public Event() {}
+
+
+	public Event(Integer id, String name, Date time) {
+		this.eventId = id;
+		this.name = name;
+		this.time = time;
+	}
+
+
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "EVENT_ID")
+	public Integer getId() {
+		return eventId;
+	}
+
+
+	@Column(name = "name")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "time")
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "GROUP_ID")	
+	public Group getGroup() {
+		return group;
+	}
+
+
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+
 
 }
