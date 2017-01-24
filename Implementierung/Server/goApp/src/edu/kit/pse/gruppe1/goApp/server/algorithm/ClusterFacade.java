065687c047e2433ed69cee97cd427fd80727f7d3
JörnK <joern.kussmaul@web.de>
2017-01-24 21:23:38 +0100

    Started working

--- Implementierung/Server/goApp/src/edu/kit/pse/gruppe1/goApp/server/algorithm/ClusterFacade.java
+++ Implementierung/Server/goApp/src/edu/kit/pse/gruppe1/goApp/server/algorithm/ClusterFacade.java
@@ -1,22 +1,72 @@
 package edu.kit.pse.gruppe1.goApp.server.algorithm;
 
-import edu.kit.pse.gruppe1.goApp.client.model.*;
+import edu.kit.pse.gruppe1.goApp.server.database.management.EventUserManagement;
+import edu.kit.pse.gruppe1.goApp.server.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.math4.ml.clustering.*;
 
 /**
- * This class is facade for the algortihm, requests the events locations from the database and calls the methods of the Clusterer and the CentralPointAlgo.
- * It also generates Points from the given coordinates, because the Algorithm need them.
+ * This class is facade for the algortihm, requests the events locations from the database and calls
+ * the methods of the Clusterer and the CentralPointAlgo. It also generates Points from the given
+ * coordinates, because the Algorithm need them.
  */
 public class ClusterFacade {
 
-	/**
-	 * This method fetches all locations from the database which belong to the event and calls the cluster algorithm to get it clustered. After that it calls the MidpointAlgo to get the midpoints from the calculated clusters.
-	 * 
-	 * @return central points of the calculated clusters
-	 * @param event List of points which should clustered and which central point should be calculated.
-	 */
-	public List<Point> getClusteredCentralPoints(Event event) {
-		// TODO - implement ClusterFacade.getClusteredCentralPoints
-		throw new UnsupportedOperationException();
-	}
+  /**
+   * This method fetches all locations from the database which belong to the event and calls the
+   * cluster algorithm to get it clustered. After that it calls the MidpointAlgo to get the
+   * midpoints from the calculated clusters.
+   * 
+   * @return central points of the calculated clusters
+   * @param event
+   *          List of points which should clustered and which central point should be calculated.
+   */
+  public List<DoublePoint> getClusteredCentralPoints(Event event) {
+    int eventId = event.getEventId();
+
+  }
+
+  private List<DoublePoint> getEventsLocations(int eventId) {
+
+    EventUserManagement management = new EventUserManagement();
+    List<User> list = management.getUsers(eventId);
+    List<DoublePoint> locations = new ArrayList<DoublePoint>(0);
+    DoublePoint userPoint;
+    Location userLocation;
+
+    for (User user : list) {
+
+      userLocation = user.getLocation();
+      if (!user.getLocation().equals(null)) {
+
+        userPoint = new DoublePoint(
+            new double[] { userLocation.getLongitude(), userLocation.getLatitude() }); // Build a
+                                                                                       // DoublePoint
+                                                                                       // from the
+                                                                                       // Location
+        locations.add(userPoint);
+      }
+
+    }
+
+    return locations;
+
+  }
+
+  private List<CentroidCluster> getClusters(List<DoublePoint> points) {
+    
+    DBSCANClusterer<DoublePoint> clusterer = new DBSCANClusterer<DoublePoint>(0.1, 2);
+    
+    
+    clusterer.cluster(points);
+    
+    
+    
+    
+    
+  }
 
 }
