1b7b6c8bc25cc171a69e31366bf6a19781c30468
Eva-Maria Neumann <upebo@student.kit.edu>
2017-01-24 23:42:58 +0100

    added LocationName, matched file from Client

--- Implementierung/Server/goApp/src/edu/kit/pse/gruppe1/goApp/server/servlet/JSONParameter.java
+++ Implementierung/Server/goApp/src/edu/kit/pse/gruppe1/goApp/server/servlet/JSONParameter.java
@@ -1,73 +1,144 @@
 package edu.kit.pse.gruppe1.goApp.server.servlet;
 
-
 /**
  * Enumerations with all possible parameter-types for the JSON-strings.
  */
 public enum JSONParameter {
-	/**
-	 * ID of the request
-	 */
-	ID("ID"),
-	/**
-	 * Error code which is 0 if no error occurred.
-	 */
-	ErrorCode("Error code"),
-	/**
-	 * ID of an user.
-	 */
-	UserID("User ID"),
-	/**
-	 * ID of a group.
-	 */
-	GroupID("Group ID"),
-	/**
-	 * ID of an event.
-	 */
-	EventID("Event ID"),
-	/**
-	 * Name of an user.
-	 */
-	UserName("User name"),
-	/**
-	 * Name of a group.
-	 */
-	GroupName("Group name"),
-	/**
-	 * Name of an event.
-	 */
-	EventName("Event name"),
-	/**
-	 * The name of the method which should be executed on the server. For example the create method of the GroupServlet.
-	 */
-	Method("Method");
 
-	private final String fieldDescription;
+  /**
+   * 
+   * ID of the request
+   * 
+   */
 
-	private JSONParameter(String description) {
-		fieldDescription = description;
-	}
+  ID("ID"),
 
-	/**
-	 * Gives the corresponding name to an enum literal. Normally something like the enum literal name.
-	 */
-	public String toString() {
-		return fieldDescription;
-	}
+  /**
+   * 
+   * Error code which is 0 if no error occurred.
+   * 
+   */
 
-	/**
-	 * Gives the corresponding enum literal to a string.
-	 *
-	 * @param s the string to which the returned JSONParameter should match
-	 * @return the JSONParameter to the string s
-	 */
-	public static JSONParameter fromString(String s) {
-		for (JSONParameter json : JSONParameter.values()) {
-			if (json.toString().equals(s)) {
-				return json;
-			}
-		}
-		return null;
-	}
+  ErrorCode("Error code"),
+
+  /**
+   * 
+   * ID of an user.
+   * 
+   */
+
+  UserID("User ID"),
+
+  /**
+   * 
+   * ID of a group.
+   * 
+   */
+
+  GroupID("Group ID"),
+
+  /**
+   * 
+   * ID of an event.
+   * 
+   */
+
+  EventID("Event ID"),
+
+  /**
+   * 
+   * Name of an user.
+   * 
+   */
+
+  UserName("User name"),
+
+  /**
+   * 
+   * Name of a group.
+   * 
+   */
+
+  GroupName("Group name"),
+
+  /**
+   * 
+   * Name of an event.
+   * 
+   */
+
+  EventName("Event name"),
+
+  /**
+   * 
+   * Time when the event starts
+   * 
+   */
+
+  EventTime("Event time"),
+
+  /**
+   * 
+   * Longitude of an location
+   * 
+   */
+
+  Longitude("Longitude"),
+
+  /**
+   * Latitude of an location
+   * 
+   */
+
+  Latitude("Latitude"),
+
+  /*
+   * name of an location
+   */
+
+  LocationName("LocationName"),
+
+  /**
+   * Event Class
+   */
+  Event("Event"),
+
+  /**
+   * 
+   * The name of the method which should be executed on the server. For example the create method of
+   * the GroupServlet.
+   * 
+   */
+
+  Method("Method");
+
+  private final String fieldDescription;
+
+  private JSONParameter(String description) {
+    fieldDescription = description;
+  }
+
+  /**
+   * Gives the corresponding name to an enum literal. Normally something like the enum literal name.
+   */
+  public String toString() {
+    return fieldDescription;
+  }
+
+  /**
+   * Gives the corresponding enum literal to a string.
+   *
+   * @param s
+   *          the string to which the returned JSONParameter should match
+   * @return the JSONParameter to the string s
+   */
+  public static JSONParameter fromString(String s) {
+    for (JSONParameter json : JSONParameter.values()) {
+      if (json.toString().equals(s)) {
+        return json;
+      }
+    }
+    return null;
+  }
 
 }
