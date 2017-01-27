7cded77f7d6e5eff229a0340c91f5894ae9d61b8
Tobias <tobias.ohlsson123@gmail.com>
2017-01-27 14:16:27 +0100

    NewEventAdapter

--- Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/view/GroupActivity.java
+++ Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/view/GroupActivity.java
@@ -5,16 +5,22 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.widget.Toast;
 import edu.kit.pse.gruppe1.goApp.client.R;
 import edu.kit.pse.gruppe1.goApp.client.databinding.GroupActivityBinding;
+import edu.kit.pse.gruppe1.goApp.client.model.Event;
 import edu.kit.pse.gruppe1.goApp.client.model.Group;
 
-public class GroupActivity extends AppCompatActivity{
+public class GroupActivity extends AppCompatActivity implements ItemClickListener{
     private GroupActivityBinding binding;
     private Group group;
+    private RecyclerView newEventRecylcerView;
+    private LinearLayoutManager eventLayoutManager;
+    private NewEventAdapter newEventAdapter;
 
     public static void start(Activity activity, Group group) {
         Intent intent = new Intent(activity, GroupActivity.class);
@@ -41,5 +47,20 @@ public class GroupActivity extends AppCompatActivity{
     @Override
     protected void onStart() {
         super.onStart();
+        newEventRecylcerView = (RecyclerView) findViewById(R.id.new_event_recycler_view);
+        newEventRecylcerView.setHasFixedSize(true);
+        eventLayoutManager = new LinearLayoutManager(this);
+        newEventRecylcerView.setLayoutManager(eventLayoutManager);
+        newEventAdapter = new NewEventAdapter(fillDataset(),this);
+        newEventRecylcerView.setAdapter(newEventAdapter);
+    }
+
+    private Event[] fillDataset() {
+        return null;
+    }
+
+    @Override
+    public void onItemClicked(int position) {
+
     }
 }
