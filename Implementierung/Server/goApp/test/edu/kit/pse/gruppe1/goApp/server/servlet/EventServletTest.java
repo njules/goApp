d9093bd12438d639697e882010cb4abd32268d86
Eva-Maria Neumann <upebo@student.kit.edu>
2017-01-29 17:14:18 +0100

    created tests for all Servlets (so far without functionality)

---
+++ Implementierung/Server/goApp/test/edu/kit/pse/gruppe1/goApp/server/servlet/EventServletTest.java
@@ -0,0 +1,34 @@
+package edu.kit.pse.gruppe1.goApp.server.servlet;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventServletTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testEventServlet() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDoGetHttpServletRequestHttpServletResponse() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDoPostHttpServletRequestHttpServletResponse() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ Implementierung/Server/goApp/test/edu/kit/pse/gruppe1/goApp/server/servlet/LoginServletTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package edu.kit.pse.gruppe1.goApp.server.servlet;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Eva-Maria
+ *
+ */
+public class LoginServletTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link edu.kit.pse.gruppe1.goApp.server.servlet.LoginServlet#LoginServlet()}.
+     */
+    @Test
+    public void testLoginServlet() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link edu.kit.pse.gruppe1.goApp.server.servlet.LoginServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)}.
+     */
+    @Test
+    public void testDoGetHttpServletRequestHttpServletResponse() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link edu.kit.pse.gruppe1.goApp.server.servlet.LoginServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)}.
+     */
+    @Test
+    public void testDoPostHttpServletRequestHttpServletResponse() {
+        fail("Not yet implemented");
+    }
+
+}
