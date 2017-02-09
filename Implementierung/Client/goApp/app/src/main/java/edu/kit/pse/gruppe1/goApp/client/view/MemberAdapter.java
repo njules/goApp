2ef287928a5f954f974933b7a8ff474a77c50bf9
Katharina Riesterer <uxecu@student.kit.edu>
2017-02-09 16:08:46 +0100

    AdminFrgment meets Service

--- Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/view/MemberAdapter.java
+++ Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/view/MemberAdapter.java
@@ -11,16 +11,20 @@ import edu.kit.pse.gruppe1.goApp.client.databinding.MemberViewBinding;
 import edu.kit.pse.gruppe1.goApp.client.model.Event;
 import edu.kit.pse.gruppe1.goApp.client.model.User;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Tobias on 27.01.2017.
  */
 
 public class MemberAdapter extends RecyclerView.Adapter<MemberAdapter.MemberViewHolder> {
-    private User[] dataset;
+    private List<User> dataset;
     private final ItemClickListener itemClickListener;
 
     public MemberAdapter(User[] userDataset, ItemClickListener icl) {
-        dataset = userDataset;
+        dataset = new ArrayList<>(Arrays.asList(userDataset));
         itemClickListener = icl;
     }
 
@@ -32,18 +36,26 @@ public class MemberAdapter extends RecyclerView.Adapter<MemberAdapter.MemberView
 
     @Override
     public void onBindViewHolder(MemberViewHolder holder, int position) {
-        final User user = dataset[position];
+        final User user = dataset.get(position);
         holder.getBinding().setUser(user);
         holder.getBinding().executePendingBindings();
     }
 
     @Override
     public int getItemCount() {
-        return dataset.length;
+        return dataset.size();
+    }
+    public void deleteItem(int position){
+        dataset.remove(position);
+        notifyItemRemoved(position);
+    }
+    public void insertItem(User user){
+        dataset.add(user);
+        notifyItemInserted(dataset.size()-1);
     }
 
     public User getItem(int position) {
-        return dataset[position];
+        return dataset.get(position);
     }
 
     public static class MemberViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
