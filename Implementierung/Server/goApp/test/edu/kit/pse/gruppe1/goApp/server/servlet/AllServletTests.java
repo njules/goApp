596016b1c5a5adcf74b8d734f74968a4dc773166
Eva-Maria Neumann <upebo@student.kit.edu>
2017-02-10 15:31:34 +0100

    added RequestSearchServlet to AllServletTests

--- Implementierung/Server/goApp/test/edu/kit/pse/gruppe1/goApp/server/servlet/AllServletTests.java
+++ Implementierung/Server/goApp/test/edu/kit/pse/gruppe1/goApp/server/servlet/AllServletTests.java
@@ -7,7 +7,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ EventServletTest.class, GroupSearchServletTest.class, GroupServletTest.class,
         LocationServletTest.class, LoginServletTest.class, ParticipateServletTest.class,
-        RequestServletTest.class, ServletUtilsTest.class, UserServletTest.class })
+        RequestServletTest.class, ServletUtilsTest.class, UserServletTest.class,
+        RequestSearchServletTest.class })
 public class AllServletTests {
 
 }
