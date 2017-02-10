c9b2827359a3e5cd322e1e9545990303bd600308
njules <julian.neubert@gmx.de>
2017-02-10 20:24:21 +0100

    Implement another test
    which of course fails

--- Implementierung/Server/goApp/test/edu/kit/pse/gruppe1/goApp/server/servlet/GroupServletTest.java
+++ Implementierung/Server/goApp/test/edu/kit/pse/gruppe1/goApp/server/servlet/GroupServletTest.java
@@ -8,10 +8,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Field;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.After;
@@ -26,7 +33,10 @@ import edu.kit.pse.gruppe1.goApp.server.database.management.EventUserManagement;
 import edu.kit.pse.gruppe1.goApp.server.database.management.GroupManagement;
 import edu.kit.pse.gruppe1.goApp.server.database.management.GroupUserManagement;
 import edu.kit.pse.gruppe1.goApp.server.database.management.UserManagement;
+import edu.kit.pse.gruppe1.goApp.server.model.Event;
 import edu.kit.pse.gruppe1.goApp.server.model.Group;
+import edu.kit.pse.gruppe1.goApp.server.model.Location;
+import edu.kit.pse.gruppe1.goApp.server.model.Status;
 import edu.kit.pse.gruppe1.goApp.server.model.User;
 
 public class GroupServletTest {
@@ -257,9 +267,87 @@ public class GroupServletTest {
         }
     }
     
+    //TODO fuck this test
     @Test
     public void testEventRequesting() {
-        fail("Not yet implemented");
+        // set up input
+        final int group = 80;
+        final int user = 77;
+        final Group randomGroup = new Group();
+        final User randomFounder = new User();
+        final Location randomPlace = new Location(0, 0, "null");
+        final Timestamp randomTime = new Timestamp(0);
+        final List<Event> fakePending = new ArrayList<Event>();
+        fakePending.add(new Event("secret meeting", randomPlace, randomTime, randomGroup, randomFounder));
+        final List<Event> fakeFriends = new ArrayList<Event>();
+        fakeFriends.add(new Event("you're not invited, go away", randomPlace, randomTime, randomGroup, randomFounder));
+        final List<Event> fakeMeeting = new ArrayList<Event>();
+        fakeMeeting.add(new Event("why do you have to make everything worse", randomPlace, randomTime, randomGroup, randomFounder));
+        // prepare input JSON parameter
+        try {
+            JSONObject json = new JSONObject();
+            json.put(JSONParameter.METHOD.toString(), JSONParameter.Methods.GET_EVENT);
+            json.put(JSONParameter.USER_ID.toString(), user);
+            json.put(JSONParameter.GROUP_ID.toString(), group);
+            jsonRequest = json.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            fail("Failed to create JSON request!\n");
+        }
+        // initialize mocking
+        try {
+            when(httpRequest.getReader()).thenReturn(request);
+            when(httpResponse.getWriter()).thenReturn(response);
+            when(request.readLine()).thenReturn(jsonRequest);
+            when(eventUserManager.getEventsByStatus(Status.INVITED, group , user)).thenReturn(fakePending);
+            when(eventUserManager.getEventsByStatus(Status.PARTICIPATE, group, user)).thenReturn(fakeFriends);
+            when(eventUserManager.getEventsByStatus(Status.STARTED, group, user)).thenReturn(fakeMeeting);
+       } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+            fail("Failed mocking!\n");
+        }
+        // call method
+        try {
+            servlet.doPost(httpRequest, httpResponse);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+            fail("Failed to post HTTP request!\n");
+        }
+        // test for correct event list
+        verify(response).println(argCap.capture());
+        List<Event> pending = new ArrayList<Event>();
+        List<Event> going = new ArrayList<Event>();
+        try {
+            JSONObject json = new JSONObject(argCap.getValue());
+            assertEquals(json.getInt(JSONParameter.ERROR_CODE.toString()), JSONParameter.ErrorCodes.OK.getErrorCode());
+            JSONArray array = json.getJSONArray(JSONParameter.NEW_EVENTS.toString());
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject jsonEvt = array.getJSONObject(i);
+                pending.add(new Event(jsonEvt.getString(JSONParameter.EVENT_NAME.toString()), null, null, null, null));
+            }
+            array = json.getJSONArray(JSONParameter.ACC_EVENTS.toString());
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject jsonEvt = array.getJSONObject(i);
+                going.add(new Event(jsonEvt.getString(JSONParameter.EVENT_NAME.toString()), null, null, null, null));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            fail("Failed to read JSON response!\n");
+        }
+        for (Event test : fakePending) {
+            assertTrue(pending.contains(test));
+            pending.remove(test);
+        }
+        assertTrue(pending.isEmpty());
+        for (Event test : fakeFriends) {
+            assertTrue(going.contains(test));
+            going.remove(test);
+        }
+        for (Event test : fakeMeeting) {
+            assertTrue(going.contains(test));
+            going.remove(test);
+        }
+        assertTrue(going.isEmpty());
     }
     
     @Test
