dde65f6fbd666a986f3262a36ea765e13de3b13e
Tobias <tobias.ohlsson123@gmail.com>
2017-02-17 14:09:35 +0100

    javadocs for model

--- Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/model/Event.java
+++ Implementierung/Client/goApp/app/src/main/java/edu/kit/pse/gruppe1/goApp/client/model/Event.java
@@ -21,38 +21,31 @@ public class Event implements Parcelable {
      * The name of an event is given by the creator.
      */
     private String name;
-
-    public Timestamp getTime() {
-        return time;
-    }
-
     /**
      * The time of an event tells when the event is starting and set by the creator of the event.
      */
     private Timestamp time;
-
-    private Group group;
     private User creator;
-    private ArrayList<Participant> participants;
-    private ArrayList<Location> clusterPoints;
     private Location location;
 
-    public User getCreator() {
-        return creator;
+    /**
+     * @param id       The Id of the event.
+     * @param name     The name of the event.
+     * @param time     The time of the event.
+     * @param location The Location of the event.
+     * @param creator  the User who created the event.
+     */
+    public Event(int id, String name, Timestamp time, Location location, User creator) {
+        this.id = id;
+        this.name = name;
+        this.time = time;
+        this.location = location;
+        this.creator = creator;
     }
 
-    public ArrayList<Participant> getParticipants() {
-        return participants;
-    }
-
-    public ArrayList<Location> getClusterPoints() {
-        return clusterPoints;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
+    /**
+     * @param in the Parcel that creates the Event.
+     */
     protected Event(Parcel in) {
         id = in.readInt();
         name = in.readString();
@@ -73,34 +66,54 @@ public class Event implements Parcelable {
         }
     };
 
+    /**
+     * @return the Id of the Event.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @return the name of the Event.
+     */
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * @return the timestamp of the Event.
+     */
+    public Timestamp getTime() {
+        return time;
     }
 
-    public void setTime(Timestamp time) {
-        this.time = time;
+    /**
+     * @return the User who created the Event.
+     */
+    public User getCreator() {
+        return creator;
     }
 
 
     /**
-     * @param id   The Id of the event.
-     * @param name The name of the event.
-     * @param time The time of the event.
+     * @return the location of the Event.
      */
-    public Event(int id, String name, Timestamp time, Location location,User creator) {
-        this.id = id;
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     * @param name the new name of the event.
+     */
+    public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * @param time the new timestamp of the Event.
+     */
+    public void setTime(Timestamp time) {
         this.time = time;
-        this.location = location;
-        this.creator = creator;
     }
 
     @Override
@@ -113,12 +126,8 @@ public class Event implements Parcelable {
         out.writeInt(id);
         out.writeString(name);
         out.writeLong(time.getTime());
-        out.writeParcelable(location,i);
-        out.writeParcelable(creator,i);
+        out.writeParcelable(location, i);
+        out.writeParcelable(creator, i);
     }
 
-    //TODO Usefull?
-    public void setClusterPoints(ArrayList<Location> clusterPoints) {
-        this.clusterPoints = clusterPoints;
-    }
 }
